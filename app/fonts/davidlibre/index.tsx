16ad23287c0a8a05c9529760c134e71fd1b6f1c3
Steven Degutis <sbdegutis@gmail.com>
2022-02-19 15:49:46 -0600

    Remove unused font.

--- app/fonts/davidlibre/index.tsx
+++
@@ -1,6 +0,0 @@
-import { makeFontComponent } from "../../util/css";
-
-export const DavidLibre = {
-  fontFamily: `'David Libre', serif`,
-  load: makeFontComponent(__dir.filesByName['david-libre.css']!),
-};
