cb87e0290bbbef9a72c3665b6ff5dd8b03981a2d
Steven Degutis <sbdegutis@gmail.com>
2022-02-19 15:39:15 -0600

    Moving home page CSS into home.css

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,8 +1,9 @@
 import { NarrowContainer } from "../../components/narrow-container/container";
-import { Page } from "../../components/page/page";
+import { Page, Stylesheet } from "../../components/page/page";
 import { renderElement } from "../../core/jsx";
 import { addRouteable } from "../../core/router";
 import { markdown } from "../../util/markdown";
+import { staticRouteFor } from "../../util/static";
 
 const content = markdown.render(__dir.filesByName['content.md']!.buffer.toString('utf8'));
 
@@ -17,6 +18,7 @@ addRouteable({
       },
       body: renderElement(<>
         <Page title="Novo Cantico">
+          <Stylesheet src={staticRouteFor(__dir.filesByName['home.css']!)} />
           <NarrowContainer style='margin-bottom:7em'>
             <h1 style='margin-bottom:0'>Novo Cantico</h1>
             {content}
