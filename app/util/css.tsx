9cfa11b8aae80e938c2f7cfb7c50185256d4e6a3
Steven Degutis <sbdegutis@gmail.com>
2022-02-19 10:30:50 -0600

    Fixed bug.

--- app/util/css.tsx
+++ app/util/css.tsx
@@ -2,12 +2,11 @@ import { Stylesheet } from "../components/page/page";
 import { baseUrl } from "../core/http-server";
 import { staticRouteFor } from "./static";
 
-export function makeFontComponent(dir: FsDir, name: string) {
+export function makeFontComponent(dir: FsDir, name: string): JSX.Component<{}> {
   const file = dir.filesByName[name]!;
   const css = file.buffer.toString('utf8').replace(/url\((.+?)\)/g, (whole, filename) => {
     return `url(${baseUrl.replace(/\/+$/, '')}${staticRouteFor(dir.filesByName[filename]!)})`;
   });
   const font = { name, buffer: Buffer.from(css) };
-  const stylesheet = <Stylesheet src={staticRouteFor(font)} />;
-  return () => stylesheet;
+  return () => <Stylesheet src={staticRouteFor(font)} />;
 }
