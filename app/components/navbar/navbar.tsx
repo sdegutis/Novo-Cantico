d509a99db13da7be522af1468808a103b2cf896b
Steven Degutis <sbdegutis@gmail.com>
2022-02-21 09:38:08 -0600

    Link to GH repo.

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -13,6 +13,7 @@ export const NavBar: JSX.Component<{}> = (attrs, children) => <>
       <a href={landingPage.route}>Novo Cantico</a>
       <a href={howItWorksPage.route}>How it works</a>
       <a href={blogIndexPage.route}>Blog posts</a>
+      <a href='https://github.com/sdegutis/Novo-Cantico'>GitHub repo</a>
       <iframe src="https://github.com/sponsors/sdegutis/button" title="Sponsor sdegutis" height="35" width="116" style="border: 0;"></iframe>
     </nav>
     <p><ViewSourceLink file={__file}>View nav-bar's source</ViewSourceLink></p>
